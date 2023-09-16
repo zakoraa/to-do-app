a351ac712e5687652c2eff8ba8b48871c969f939
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-16 22:50:02 +0700

    feat: add feature bottom sheet to home page

--- lib/module/home/widgets/floating_button_widget.dart
+++ lib/module/home/widgets/floating_button_widget.dart
@@ -12,7 +12,40 @@ class FloatingButtonWidget extends StatelessWidget {
         bottom: 20,
         right: 10,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            showBottomSheet(
+              context: context,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              builder: (context) {
+                return Container(
+                  height: 500,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    color: Color.fromARGB(255, 20, 20, 20),
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Icon(
+                            Icons.arrow_back,
+                            size: 35,
+                          )),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
           backgroundColor: CustomColor.secondaryColor,
           child: const Center(
             child: Icon(
