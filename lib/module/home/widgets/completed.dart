144101fa8aa185fc97f3beda8e3822efc3342989
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-16 21:34:22 +0700

    style: upddate home page view

--- lib/module/home/widgets/completed.dart
+++ lib/module/home/widgets/completed.dart
@@ -1,22 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/module/home/widgets/completed_content.dart';
+import '../../../shared/themes/color.dart';
+import 'check_box_active.dart';
 
 class Completed extends StatelessWidget {
   const Completed({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           "Completed",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
         ),
-        SizedBox(
+        const SizedBox(
           height: 10.0,
         ),
-        CompletedContent(),
+        Container(
+          padding: const EdgeInsets.only(bottom: 10),
+          width: MediaQuery.of(context).size.width,
+          child: const Row(
+            children: [
+              SizedBox(
+                width: 15.0,
+              ),
+              CheckBoxActive(),
+              SizedBox(
+                width: 20.0,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "Belajar Dart",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "💻 Programming",
+                    style: TextStyle(color: CustomColor.secondaryTextColor),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Completed on April 4, 2005",
+                        style: TextStyle(
+                            color: CustomColor.secondaryTextColor,
+                            fontSize: 10))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
--- lib/module/home/widgets/incomplete.dart
+++ lib/module/home/widgets/incomplete.dart
@@ -1,22 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:todoapp/module/home/widgets/incomplete_content.dart.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../shared/themes/color.dart';
+import '../businessLogic/cubit/check_box.dart';
+import 'check_box_active.dart';
+import 'check_box_inactive.dart';
 
 class Incomplete extends StatelessWidget {
   const Incomplete({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           "Incomplete",
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
         ),
-        SizedBox(
+        const SizedBox(
           height: 20.0,
         ),
-        IncompleteContent()
+        Container(
+          padding: const EdgeInsets.only(bottom: 10),
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 15.0,
+              ),
+              BlocProvider(
+                create: (context) => CheckCubit(),
+                child: BlocBuilder<CheckCubit, CheckState>(
+                  builder: (context, state) {
+                    return GestureDetector(
+                        onTap: () {
+                          context.read<CheckCubit>().checkedBox();
+                        },
+                        child: state.isCheck
+                            ? const CheckBoxActive()
+                            : const CheckBoxInactive());
+                  },
+                ),
+              ),
+              const SizedBox(
+                width: 20.0,
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Tugas Akhir PBO",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "Deadline : Sep 15, 2023",
+                    style: TextStyle(
+                        fontSize: 10, color: CustomColor.secondaryTextColor),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    "📒 Tugas",
+                    style: TextStyle(color: CustomColor.secondaryTextColor),
+                  ),
+                ],
+              ),
+              const Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [Icon(Icons.edit)],
+                ),
+              ),
+            ],
+          ),
+        )
       ],
     );
   }
