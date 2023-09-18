ee2bd7dc1727beac99e23e11ba1e1e540d9e5f20
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-18 17:06:35 +0700

    style: update inactive checkbox color when light theme mode

--- lib/module/home/widgets/check_box_inactive.dart
+++ lib/module/home/widgets/check_box_inactive.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todoapp/module/home/businessLogic/cubit/theme.dart';
 
 import '../../../shared/themes/color.dart';
 
@@ -7,12 +9,16 @@ class CheckBoxInactive extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 20,
-      width: 20,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: CustomColor.secondaryTextColor,
+    return BlocBuilder<ThemeCubit, ThemeState>(
+      builder: (context, state) => Container(
+        height: 20,
+        width: 20,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: state.isLight
+              ? const Color.fromARGB(255, 180, 180, 180)
+              : CustomColor.secondaryTextColor,
+        ),
       ),
     );
   }
