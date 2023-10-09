93a13c0ea54b48c8f0a332580ed995d504c7ef39
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-09 22:08:18 +0700

    update to do model

--- lib/model/to_do_model.dart
+++ lib/model/to_do_model.dart
@@ -1,13 +1,53 @@
-enum ToDoType { tugas, programming }
+enum ToDoType { tugas, programming, others }
 
 class ToDo {
-  String id,title, description, createdTime;
-  ToDoType? type;
+  final String title, createdTime;
+  final ToDoType? type;
+  bool isDone;
 
   ToDo(
-      {required this.id,
-      required this.title,
-      required this.description,
+      {required this.title,
       required this.createdTime,
-      required this.type});
+      required this.type,
+      this.isDone = false});
+
+  ToDo copyWith({
+    String? title,
+    String? createdTime,
+    ToDoType? type,
+    bool? isDone,
+  }) {
+    return ToDo(
+        title: title ?? this.title,
+        createdTime: createdTime ?? this.createdTime,
+        type: type ?? type,
+        isDone: isDone ?? this.isDone);
+  }
+
+  factory ToDo.fromJson(Map<String, dynamic> json) {
+    return ToDo(
+        title: json["title"],
+        createdTime: json["createdTime"],
+        type: json["type"],
+        isDone: json["isDone"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "createdTime": createdTime,
+      "type": type,
+      "isDone": isDone,
+    };
+  }
+
+  @override
+  String toString() {
+    return """Todo: {
+      title: $title\n
+      createdTime: $createdTime\n
+      type: $type\n
+      isDone: $isDone\n
+    }""";
+  }
 }
