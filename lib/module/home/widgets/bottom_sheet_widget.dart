85464e756caf7e952fe5568658aba74f70046afc
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-11 07:57:01 +0700

    fix: bug on date picker

--- lib/module/home/widgets/bottom_sheet_widget.dart
+++ lib/module/home/widgets/bottom_sheet_widget.dart
@@ -52,99 +52,105 @@ class BottomSheetWidget extends StatelessWidget {
       height: 400,
       child: ListView(physics: const BouncingScrollPhysics(), children: [
         BlocBuilder<ThemeCubit, ThemeState>(
-          builder: (context, state) => Container(
-            height: 400,
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-              color: Color.fromARGB(255, 20, 20, 20),
-            ),
-            width: MediaQuery.of(context).size.width,
-            child: BlocBuilder<ToDoBloc, ToDoState>(
-              builder: (context, state) => Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(
-                          Icons.keyboard_arrow_down_rounded,
-                          color: Colors.white,
-                          size: 40,
-                        ),
-                      ),
-                      SizedBox(
-                          width: MediaQuery.of(context).size.width - 140,
-                          child: Center(
-                              child: Text(
-                            isAddToDoList
-                                ? "Add Your To-Do List "
-                                : "Edit Your To-Do List ",
-                            textAlign: TextAlign.center,
-                            style: const TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16),
-                          ))),
-                      const SizedBox(
-                        width: 50.0,
-                      ),
-                    ],
+            builder: (context, state) => Container(
+                  height: 400,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                    color: Color.fromARGB(255, 20, 20, 20),
                   ),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                  CustomTextField(
-                    labelText: "List Title",
-                    controller: titleController,
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  const ChooseDeadline(),
-                  const SizedBox(
-                    height: 15.0,
-                  ),
-                  const DropDownButtonType(),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  BlocBuilder<DropDownCubit, DropDownState>(
-                    builder: (context, state) => Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: BlocBuilder<DatePickerCubit, DatePickerState>(
-                        builder: (context, datePickerState) => OutlinedButton(
-                            style: const ButtonStyle(
-                              side: MaterialStatePropertyAll(BorderSide(
-                                  color: CustomColor.secondaryTextColor)),
-                              splashFactory: NoSplash.splashFactory,
+                  width: MediaQuery.of(context).size.width,
+                  child: BlocBuilder<ToDoBloc, ToDoState>(
+                    builder: (context, state) =>
+                        BlocBuilder<DatePickerCubit, DatePickerState>(
+                      builder: (context, datePickerState) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  datePickerState.selectedDate = today;
+                                  Navigator.pop(context);
+                                },
+                                child: const Icon(
+                                  Icons.keyboard_arrow_down_rounded,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                              ),
+                              SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width - 140,
+                                  child: Center(
+                                      child: Text(
+                                    isAddToDoList
+                                        ? "Add Your To-Do List "
+                                        : "Edit Your To-Do List ",
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ))),
+                              const SizedBox(
+                                width: 50.0,
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 40.0,
+                          ),
+                          CustomTextField(
+                            labelText: "List Title",
+                            controller: titleController,
+                          ),
+                          const SizedBox(
+                            height: 30.0,
+                          ),
+                          const ChooseDeadline(),
+                          const SizedBox(
+                            height: 15.0,
+                          ),
+                          const DropDownButtonType(),
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          BlocBuilder<DropDownCubit, DropDownState>(
+                            builder: (context, state) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: OutlinedButton(
+                                  style: const ButtonStyle(
+                                    side: MaterialStatePropertyAll(BorderSide(
+                                        color: CustomColor.secondaryTextColor)),
+                                    splashFactory: NoSplash.splashFactory,
+                                  ),
+                                  onPressed: () {
+                                    addToDo(
+                                        ToDo(
+                                            title: titleController.text,
+                                            createdTime:
+                                                datePickerState.selectedDate,
+                                            type: state.selectedType),
+                                        context);
+                                    titleController.clear();
+                                    datePickerState.selectedDate = today;
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Center(
+                                    child: Text("Submit"),
+                                  )),
                             ),
-                            onPressed: () {
-                              addToDo(
-                                  ToDo(
-                                      title: titleController.text,
-                                      createdTime: datePickerState.selectedDate,
-                                      type: state.selectedType),
-                                  context);
-                              titleController.clear();
-                              datePickerState.selectedDate = today;
-                              Navigator.pop(context);
-                            },
-                            child: const Center(
-                              child: Text("Submit"),
-                            )),
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ),
+                ))
       ]),
     );
   }
