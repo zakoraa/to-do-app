0860922a93a1ae77ed1451f0a33321abe35dc65a
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-10-11 23:31:28 +0700

    style: add dialog theme

--- lib/shared/themes/theme.dart
+++ lib/shared/themes/theme.dart
@@ -3,12 +3,23 @@ import 'package:todoapp/shared/themes/color.dart';
 
 class CustomTheme {
   static ThemeData get darkTheme => ThemeData.dark().copyWith(
-    primaryColor: CustomColor.backgroundColor,
+      dialogTheme: const DialogTheme(
+          titleTextStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.white,
+      )),
+      primaryColor: CustomColor.backgroundColor,
       scaffoldBackgroundColor: CustomColor.primaryColor,
-      textTheme: const TextTheme(bodyMedium: TextStyle(fontSize: 12, color: Colors.white)));
+      textTheme: const TextTheme(
+          bodyMedium: TextStyle(fontSize: 12, color: Colors.white)));
 
   static ThemeData get lightTheme => ThemeData.light().copyWith(
-    primaryColor: Colors.white,
+      dialogTheme: const DialogTheme(
+          titleTextStyle: TextStyle(
+        fontSize: 16,
+        color: Colors.black,
+      )),
+      primaryColor: Colors.white,
       datePickerTheme: const DatePickerThemeData(
           backgroundColor: Colors.white,
           headerBackgroundColor: CustomColor.secondaryTextColor),
